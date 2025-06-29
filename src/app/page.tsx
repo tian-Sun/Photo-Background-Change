9ad021d9b43b136f66e03b638693700b94afa1a2
GeekGallifrey <GeekGallifrey@users.noreply.github.com>
2025-06-29 15:43:55 +0800

    modify homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,7 @@ import ExamplesGallery from "@/components/ExamplesGallery";
 import BlogPreview from "@/components/BlogPreview";
 import MainContent from "@/components/MainContent";
 import ScrollToHash from "@/components/ScrollToHash";
+import HeadshotGenerator from "@/components/ai-generator/HeadshotGenerator";
 
 interface Background {
   id: string;
@@ -28,8 +29,10 @@ export default function Home() {
     <main>
       <ScrollToHash />
       <div className="bg-gradient-to-br from-purple-50 to-purple-100">
-        <MainContent />
-        <HeroSection />
+       
+        {/* <MainContent /> */}
+         <HeroSection />
+         <HeadshotGenerator />
         <ExamplesGallery />
         <WhatIs />
         <KeyFeatures />
